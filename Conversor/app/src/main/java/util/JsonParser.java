550f3db3d4913025fff64ae0ea7ed9c40222880f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-03 18:09:28 -0300

    JSON parser has been created

---
+++ Conversor/app/src/main/java/util/JsonParser.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author amand
+ */
+public class JsonParser {
+    public static String JsonEmString(BufferedReader buffereReader) throws IOException{
+        String resposta, jsonEmString = "";
+        while((resposta = buffereReader.readLine()) != null){
+            jsonEmString += resposta;
+        }
+        return jsonEmString;
+    }
+}
